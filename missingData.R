c3802e9ed0e7fc86da313d0bc8734464c893c0f8
Jacky Zhang <jakzhang@ucdavis.edu>
2017-05-13 15:17:19 -0700

    First commit

---
+++ missingData.R
@@ -0,0 +1,25 @@
+missingData<-function(data, only.missing=TRUE, plot.color="coral1"){
+  
+  library(ggplot2)
+  library(dplyr)
+  #Calculate percent missing data for every column in input data frame
+  miss_pct<-purrr::map_dbl(data, function(x){round((sum(is.na(x)) / length(x)) * 100, 1)})
+  
+  #Subset miss_pct>0
+  if(only.missing){
+    miss_pct<-miss_pct[miss_pct > 0]
+  }
+  
+  missing.df<-data.frame(pct.missing=miss_pct, var=names(miss_pct), row.names=NULL)
+  
+  
+  #Plot percent of missing data for variables, in descending order
+  df %>%
+    ggplot(aes(x=reorder(var, -miss), y=miss)) + 
+    geom_bar(stat='identity', fill=plot.color) +
+    labs(x='', y='% missing', title='Percent missing data by feature') +
+    theme(axis.text.x=element_text(angle=90, hjust=1))
+  
+  #Same thing in tabular form
+  return (dplyr::arrange(missing.df, desc(pct.missing)))
+}
