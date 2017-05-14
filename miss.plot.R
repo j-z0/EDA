3115fbcf262056429a1c12d4d1f4d54740c0d2eb
Jacky Zhang <jakzhang@ucdavis.edu>
2017-05-14 16:55:09 -0700

    Update miss.plot.R

--- miss.plot.R
+++ miss.plot.R
@@ -1,5 +1,4 @@
 miss.plot<-function(data, only.missing=TRUE, plot.color="coral1"){
-  
   library(ggplot2)
   library(dplyr)
   #Calculate percent missing data for every column in input data frame
@@ -14,12 +13,25 @@ miss.plot<-function(data, only.missing=TRUE, plot.color="coral1"){
   
   
   #Plot percent of missing data for variables, in descending order
-  missing.df %>%
+  miss.plot<-missing.df %>%
     ggplot(aes(x=reorder(var, -pct.missing), y=pct.missing)) + 
     geom_bar(stat='identity', fill=plot.color) +
     labs(x='', y='% missing', title='Percent missing data by feature') +
     theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
   
+  return(miss.plot)
+}
+
+miss.table<-function(data){
+  library(ggplot2)
+  library(dplyr)
+  #Calculate percent missing data for every column in input data frame
+  miss_pct<-purrr::map_dbl(data, function(x){round((sum(is.na(x)) / length(x)) * 100, 1)})
+  
+  #Subset miss_pct>0
+  miss_pct<-miss_pct[miss_pct > 0]
+  
+  missing.df<-data.frame(var=names(miss_pct), pct.missing=miss_pct, row.names=NULL)
   #Same thing in tabular form
   return (dplyr::arrange(missing.df, desc(pct.missing)))
 }
