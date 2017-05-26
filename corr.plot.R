1359c7b247e8b05313dc69909a429bbe5c68ffca
Jacky Zhang <jakzhang@ucdavis.edu>
2017-05-26 14:47:38 -0700

    Update corr.plot

--- corr.plot.R
+++ corr.plot.R
@@ -1,14 +1,14 @@
-corr.plot<-function(data, outcome=NULL, lim=40, absolute=FALSE, col=c("cornflowerblue", "coral1")){
+corr.plot<-function(data, outcome=NULL, lim=50, sort="abs", col=c("cornflowerblue", "coral1")){
   library(dplyr)
   options(scipen = 999)
   
   #Subset numeric columns
   col.class<-sapply(data, class)
-  col.class.nonchar<-names(which(col.class!="character"))
+  col.class.nonchar<-names(which(col.class!="character"&col.class!="factor"))
   data.nonchar<-data[,col.class.nonchar]
   
   #Calculate correlation matrix
-  cor.matrix<-(cor(data.nonchar, data.nonchar, use="complete.obs"))
+  cor.matrix<-cor(data.nonchar, data.nonchar, use="complete.obs")
   
   #Generate df from half matrix without diag
   cor.df<-data.frame(row=rownames(cor.matrix)[row(cor.matrix)[upper.tri(cor.matrix)]], 
@@ -19,9 +19,8 @@ corr.plot<-function(data, outcome=NULL, lim=40, absolute=FALSE, col=c("cornflowe
     mutate(abs=abs(corr),
            colour=ifelse(corr>0, "positive", "negative"),
            label=paste0(row, " -- ", col)) %>%
-    arrange(desc(abs)) %>%
     head(lim) %>%
-    ggplot(aes(label, corr))+
+    ggplot(aes(reorder(label, get(sort)), corr))+
     geom_bar(stat="identity", position="identity", aes(fill=colour))+
     theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))+
     scale_fill_manual(values=c(positive=col[1],negative=col[2]))+
